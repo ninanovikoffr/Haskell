b64efcc775e2a0f45a960f5822dd27c8ebcab121
ninanovikoffr <ninanovikoffr@gmail.com>
2025-06-11 12:46:09 -0300

    funcao concatena

--- trabalho.hs
+++ trabalho.hs
@@ -3,3 +3,14 @@ insere_no_fim :: t -> [t] -> [t]
 insere_no_fim x [] = [x]
 insere_no_fim x (c:r)
     | otherwise = c : insere_no_fim x r
+
+-- (5) concatena: junta os elementos da primeira lista com os da segunda
+concatena :: [t] -> [t] -> [t]
+concatena l1 l2
+    | null_lista l1 = l2
+    | otherwise     = primeiro : concatena resto l2
+  where
+    null_lista [] = True
+    null_lista _  = False
+    primeiro : resto = l1
+
